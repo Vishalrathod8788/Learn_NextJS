9afda4b2f2968342e502284cb2697f13de1db93b
Vishal Rathod <vishalrathod1562004@gmail.com>
2025-06-28 11:36:54 +0530

    feat: add Roboto font import and configuration to RootLayout

--- app/(users)/layout.js
+++ app/(users)/layout.js
@@ -1,10 +1,15 @@
+import {Roboto} from "next/font/google";
 import Navigation from "@/components/Navigation";
 import "@/app/globals.css";
+
+const font = Roboto({
+  subsets: ["latin"],
+})
 
 export default function RootLayout({ children }) {
   return (
     <html>
-      <body>
+      <body className={font.className}>
         <Navigation />        
         {children}
       </body>
