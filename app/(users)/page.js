85f79c4457510feac79725c75be377d619e5c872
Vishal Rathod <vishalrathod1562004@gmail.com>
2025-06-30 17:16:44 +0530

    fix: restore metadata structure in layout.js and remove unused metadata from page.js

--- app/(users)/page.js
+++ app/(users)/page.js
@@ -1,18 +1,3 @@
-export const metadata = {
-  title:{
-    default: "Home Page / Vishal Rathod",
-    template: "%s / Vishal Rathod",
-  },
-  description: "This is the home page of our website",
-  keywords: ["home, nextjs, react", "vishal rathod", "web development"],
-  icons: {
-    icon: "vishalrathod.jpg",
-  },
-  author: {
-    name: "Vishal Rathod",
-    url: "https://vishalrathod.com",
-  },
-}
 const Home = () => {
   return <h1 className="font-roboto">Hello, welcome to our website</h1>
 }
--- app/layout.js
+++ app/layout.js
@@ -1,6 +1,18 @@
+import "@/app/globals.css";
 import {Roboto, Work_Sans} from "next/font/google";
 import { Work_Sans } from "next/font/google";
-import "@/app/globals.css";
+
+export const metadata = {
+  title: {
+    default: "Home Page / Vishal Rathod",
+    template: "%s / Vishal Rathod",
+  },
+  description: "This is the home page of our website",
+  keywords: ["home, nextjs, react", "vishal rathod", "web development"],
+  icons: {
+    icon: "/vishalrathod.jpg", // Correct way to reference the icon
+  },
+}
 
 const font = Roboto({
   subsets: ["latin"],
