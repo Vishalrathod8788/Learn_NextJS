4e9b3561dabc1d7c46a6777a5a9c8a82688570a4
Vishal Rathod <vishalrathod1562004@gmail.com>
2025-07-01 17:26:05 +0530

    feat: refactor data fetching in ClientComp to retrieve all posts

--- app/(users)/clientcomp/page.jsx
+++ app/(users)/clientcomp/page.jsx
@@ -6,26 +6,43 @@ const ClientComp = () => {
 
     const [postData, setPostData] = useState([]);
 
-    const fetchData = async () => {
-        const URL = "https://jsonplaceholder.typicode.com/posts/1";
-        try {
-            const res = await fetch(URL);
-            const data = await res.json();
-            console.log(data);
-            return data;
-        } catch (error) {
-            console.log("Error fetching data:", error);
-            
-        }
-    }
+
     useEffect(() => {
+        const fetchData = async () => {
+            const URL = "https://jsonplaceholder.typicode.com/posts/";
+            try {
+                const res = await fetch(URL);
+                const data = await res.json();
+
+                console.log(data);
+                setPostData(data);
+
+                return data;
+            } catch (error) {
+                console.log("Error fetching data:", error);
+
+            }
+        }
         fetchData();
     }, [])
-    
-    return<>
+
+    return <>
         <h1 className="font-roboto">Client Component</h1>
         <p>This is a client component.</p>
-        <button className="bg-blue-400 text-white p-5" onClick={() => alert("Hello from Client Component!")}>Click Me</button>
+        <button
+            className="bg-blue-400 text-white p-5"
+            onClick={() => alert("Hello from Client Component!")}>
+            Click Me
+        </button>
+
+        <ul className="grid grid-cols-3 gap-5">
+            {
+                postData.map((curEle, index) => {
+                    return (<li key={index}>{curEle.body}</li>)
+                })
+            }
+        </ul>
+
     </>
 }
 export default ClientComp;
