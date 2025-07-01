60b4a189c33e0df87a319c1080a28394527dc8a2
Vishal Rathod <vishalrathod1562004@gmail.com>
2025-07-01 17:19:18 +0530

    feat: implement data fetching in ClientComp component

--- app/(users)/clientcomp/page.jsx
+++ app/(users)/clientcomp/page.jsx
@@ -1,4 +1,27 @@
+"use client";
+
+import { useEffect, useState } from "react";
+
 const ClientComp = () => {
+
+    const [postData, setPostData] = useState([]);
+
+    const fetchData = async () => {
+        const URL = "https://jsonplaceholder.typicode.com/posts/1";
+        try {
+            const res = await fetch(URL);
+            const data = await res.json();
+            console.log(data);
+            return data;
+        } catch (error) {
+            console.log("Error fetching data:", error);
+            
+        }
+    }
+    useEffect(() => {
+        fetchData();
+    }, [])
+    
     return<>
         <h1 className="font-roboto">Client Component</h1>
         <p>This is a client component.</p>
