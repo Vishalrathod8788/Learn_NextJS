b110249d7a9b714aacad3cc215c91092f9801444
Vishal Rathod <vishalrathod1562004@gmail.com>
2025-07-03 23:00:42 +0530

    feat: implement DatafetchServer component for fetching and displaying user data based on name

---
+++ app/(users)/datafetch/servercomp/page.jsx
@@ -0,0 +1,15 @@
+const DatafetchServer = async (props) => {
+  const searchParams = await props.searchParams;
+  const userName = searchParams.name;
+  const res = await fetch(`https://api.genderize.io/?name=${userName}`);
+  const data = await res.json();
+  console.log(data);
+
+  return (
+    <>
+      <h1>Data Fetching = {data.name}</h1>
+    </>
+  );
+};
+
+export default DatafetchServer;
